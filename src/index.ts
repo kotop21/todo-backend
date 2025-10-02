a8034f80b36a7c4c7f71eadad851ed89fb27b0f0
Victor <72082776+kotop21@users.noreply.github.com>
2025-10-02 18:19:11 +0300

    modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+import express from "express";
 import { app } from "./app.js";
-import serverless from "serverless-http";
+import { db } from "./service/index-database.js";
+const port: number = 3000;
 
-export default serverless(app);
+
+async function main() {
+  app.listen(port, () => {
+    console.log(`Слушаем порт: ${port}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
