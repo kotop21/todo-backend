680f412c11927ff4e47855ebc96f4f62015de0f2
Victor <72082776+kotop21@users.noreply.github.com>
2025-10-08 20:02:05 +0300

    fix: refreshToken

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./swagger.json" with { type: "json" };
 import cookieParser from 'cookie-parser';
 import { errorHandler } from "./middlawes/error-handler.js";
+import cors from 'cors';
 
 import router from "./routes/index.js"
 
@@ -15,6 +16,12 @@ const swaggerOptions = {
     supportedSubmitMethods: []
   },
 };
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
+
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerOptions));
 
--- src/service/user/database/user-refresh-token.ts
+++ src/service/user/database/user-refresh-token.ts
@@ -7,9 +7,9 @@ export const createRefreshToken = async () => {
   const refreshTokenExpiresAt = new Date(new Date().setDate(new Date().getDate() + 30));
 
   return {
-    refreshToken: refreshToken,
-    hashedToken: hashedToken,
-    refreshTokenExpiresAt: refreshTokenExpiresAt,
+    refreshToken,
+    hashedToken,
+    refreshTokenExpiresAt,
   };
 };
 
@@ -24,20 +24,20 @@ export const updateRefreshToken = async (userIdIn?: number) => {
         where: { id: userIdIn },
         data: {
           refreshToken: hashedToken,
-          refreshTokenExpiresAt: refreshTokenExpiresAt,
+          refreshTokenExpiresAt,
         },
       });
     }
 
     return {
-      message: "Refresh Token has bin created",
-      refreshToken: refreshToken,
-      hashedToken: hashedToken,
-      refreshTokenExpiresAt: refreshTokenExpiresAt,
+      message: "Refresh Token has been created",
+      refreshToken,
+      hashedToken,
+      refreshTokenExpiresAt,
     };
   } catch (err: any) {
-    console.error("Error update User Reftesh Token:", err);
-    const error: any = new Error("Failed to update User Reftesh Token");
+    console.error("Error updating User Refresh Token:", err);
+    const error: any = new Error("Failed to update User Refresh Token");
     error.statusCode = 400;
     throw error;
   }
@@ -46,4 +46,4 @@ export const updateRefreshToken = async (userIdIn?: number) => {
 export const verifyRefreshToken = (refreshToken: string, hashedToken: string): boolean => {
   const hashedInput = crypto.createHash("sha256").update(refreshToken).digest("hex");
   return hashedInput === hashedToken;
-}
+};
